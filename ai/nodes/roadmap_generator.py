e8af5836038f0d1d2a1b17592960d8224b6a5ae4
parthmandore <146573878+parthmandore@users.noreply.github.com>
2026-06-23 14:06:59 +0530

    Add roadmap_generator.py to generate a roadmap

---
+++ ai/nodes/roadmap_generator.py
@@ -0,0 +1,15 @@
+def generate_roadmap(state):
+
+    return {
+        "roadmap":
+        """
+        Month 1:
+        Docker
+
+        Month 2:
+        AWS
+
+        Month 3:
+        Kubernetes
+        """
+    }
