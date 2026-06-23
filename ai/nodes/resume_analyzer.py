19c6799a5949e11491f158dbb8797e4dc6bfdd10
parthmandore <146573878+parthmandore@users.noreply.github.com>
2026-06-23 14:04:48 +0530

    Add resume analysis function to analyze_resume.py

---
+++ ai/nodes/resume_analyzer.py
@@ -0,0 +1,15 @@
+def analyze_resume(state):
+
+    resume_text = state["resume_text"]
+
+    return {
+        "skills": [
+            "Python",
+            "SQL",
+            "React"
+        ],
+        "strengths": [
+            "Projects",
+            "Problem Solving"
+        ]
+    }
